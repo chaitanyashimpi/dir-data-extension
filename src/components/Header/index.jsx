b4c307a8d59c8141f89a9de3698d685a1f7d2926
Chaitanya Shimpi <chaitanyashimpi2001@gmail.com>
2024-07-26 06:50:44 +0000

    Created React project and created a frame

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import './header.css'
+
+const Header = () => {
+    return (
+        <header>
+            <h2>Hello</h2>
+            <h3>Logo</h3>
+        </header>
+    );
+}
+
+export default Header;
